82402f94f527fdac5409e79be232548598829a19
Will Kessler <willkessler@gmail.com>
2024-03-17 21:17:35 -0700

    testing stackblitz url params

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,25 +4,43 @@ import RouterComponent from './Router';
 
 const App = () => {
 
-  // In normal usage, you'd probably initialize TINAD here. However, because the filesystem
-  // at codesandbox.io is by default read-only, we have you initialize TINAD through the text input
-  // on the About page.
-/*
-    const tinadConfig = { 
-        // This can hold whatever end user id you want to use to distinguish individual users. 
-        userId: import.meta.env.VITE_TINAD_ENDUSER_ID,
-        // Put your API key in the environment file .env so it can be picked up here.
-        apiKey: import.meta.env.VITE_TINAD_API_KEY,
-        // For production, do not pass this in and TINAD will default to the production API endpoint.
-        apiBaseUrl: import.meta.env.VITE_API_BASE_URL, 
+    // In normal usage, you'd probably initialize TINAD here. However, because the filesystem
+    // at codesandbox.io is by default read-only, we have you initialize TINAD through the text input
+    // on the About page.
+    /*
+      const tinadConfig = { 
+      // This can hold whatever end user id you want to use to distinguish individual users. 
+      userId: import.meta.env.VITE_TINAD_ENDUSER_ID,
+      // Put your API key in the environment file .env so it can be picked up here.
+      apiKey: import.meta.env.VITE_TINAD_API_KEY,
+      // For production, do not pass this in and TINAD will default to the production API endpoint.
+      apiBaseUrl: import.meta.env.VITE_API_BASE_URL, 
+      };
+      initTinadSDK(tinadConfig);
+    */
+
+    const getQueryParam = (param:string) => {
+        const search = window.location.search.substring(1); // Remove '?' prefix
+        const params = search.split('&');
+
+        for (let i = 0; i < params.length; i++) {
+            const pair = params[i].split('=');
+            if (decodeURIComponent(pair[0]) === param) {
+                return decodeURIComponent(pair[1]);
+            }
+        }
+
+        return null;
     };
-    initTinadSDK(tinadConfig);
-*/
+
+    const apiKey = getQueryParam('apiKey');
+    console.log('apiKey from URL:', apiKey);
+    
 
     return (
         <>
             <RouterComponent />
-        </>
+            </>
     );
 }
 
